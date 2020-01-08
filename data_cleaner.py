1474edb2c7625778f97f23c3acb295ca5d7c36cd
Jonghoon Lee <jayardlee@gmail.com>
2020-01-08 09:58:44 -0600

    v0.02; visuals and the model has been updated

--- data_cleaner.py
+++ data_cleaner.py
@@ -1,6 +1,16 @@
-import pandas as pd
+import scipy.stats as stats
 
 
-def basic_csv(path):
-    df = pd.read_csv(path)
-    return df
+def boxcox_trans(data, col_name, remove_col=True):
+    """
+    Conducts boxcox transformation without a hassle.
+    data = dataframe
+    col_list = a list of column names that needs to be transformed.
+    remove_col = removes the column being transformed.
+    """
+    if remove_col:
+        data[f'boxcox_{col_name}'] = stats.boxcox(data[col_name])[0]
+        data.drop(col_name,axis=1,inplace=True)
+    else:
+        data[f'boxcox_{col_name}'] = stats.boxcox(data[col_name])[0]
+    return data
